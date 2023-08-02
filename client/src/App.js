9fadff68d5093bd60435f281e2f79f9aa39baec4
seoeongeueun <erin9901@gmail.com>
2023-08-02 21:08:01 +0900

    heart balloon and enlarge animation

--- client/src/App.js
+++ client/src/App.js
@@ -1,11 +1,40 @@
+import Flag from "./images/flag.png";
+
 function App() {
   return (
     <main>
-      <div className="balloon" style={{ "--initial-rotation": "150deg" }}>
+      <div
+        className="balloon"
+        style={{
+          "--initial-rotation": "150deg",
+          top: "90vh",
+          left: "30vw",
+          "--initial-scale": "1.5",
+        }}
+      >
         <div className="roundShape">
           <div className="balloonTip" />
         </div>
       </div>
+
+      <div
+        className="heartBalloon"
+        style={{
+          "--initial-scale": "15",
+          top: "65vh",
+          left: "65vw",
+          "--initial-rotation": "33deg",
+        }}
+      >
+        <div className="heartBorder" style={{ transform: "scale(1.05)" }} />
+        <div className="heart">
+          <div style={{ zIndex: 1 }} className="heartBalloonText">
+            <span>~70%</span>
+            <span>SALE</span>
+          </div>
+        </div>
+      </div>
+      <img src={Flag} alt="flag" className="flag" />
     </main>
   );
 }
