e3f981a778b7d28b791018f49f668a0973608857
seoeongeueun <erin9901@gmail.com>
2023-08-02 22:14:29 +0900

    cloud shape

--- client/src/App.js
+++ client/src/App.js
@@ -35,6 +35,188 @@ function App() {
         </div>
       </div>
       <img src={Flag} alt="flag" className="flag" />
+      <div
+        className="cloud"
+        style={{ top: "72vh", left: "-4vw", transform: "scale(0.9)" }}
+      >
+        <div
+          className="cloudHalf"
+          style={{ marginBottom: "-6rem", marginLeft: "-1rem" }}
+        >
+          <div className="cloudShape2" style={{ marginRight: "-15px" }} />
+          <div className="cloudShape3" />
+          <div className="cloudShape3" style={{ marginBottom: "-10px" }} />
+        </div>
+        <div className="cloudHalf">
+          <div className="cloudShape4" />
+          <div className="cloudShape1" />
+          <div className="cloudShape1" />
+          <div className="cloudShape1" style={{ marginBottom: "10px" }} />
+        </div>
+      </div>
+      <div
+        className="cloud"
+        style={{
+          top: "38vh",
+          left: "8vw",
+          transform: "scale(0.4)",
+          filter: "blur(5px)",
+        }}
+      >
+        <div
+          className="cloudHalf"
+          style={{ marginBottom: "-6rem", marginLeft: "-1rem" }}
+        >
+          <div className="cloudShape2" style={{ marginRight: "-15px" }} />
+          <div className="cloudShape3" />
+          <div className="cloudShape3" style={{ marginBottom: "-10px" }} />
+        </div>
+        <div className="cloudHalf">
+          <div className="cloudShape4" />
+          <div className="cloudShape1" />
+          <div className="cloudShape1" />
+          <div className="cloudShape1" style={{ marginBottom: "10px" }} />
+        </div>
+      </div>
+      <div
+        className="cloud"
+        style={{ top: "90vh", left: "15vw", transform: "scale(0.9)" }}
+      >
+        <div
+          className="cloudHalf"
+          style={{ marginBottom: "-6rem", marginLeft: "-1rem" }}
+        >
+          <div className="cloudShape2" style={{ marginRight: "-15px" }} />
+          <div className="cloudShape3" />
+          <div className="cloudShape3" style={{ marginBottom: "-10px" }} />
+        </div>
+        <div className="cloudHalf">
+          <div className="cloudShape4" />
+          <div className="cloudShape1" />
+          <div className="cloudShape1" />
+          <div className="cloudShape1" style={{ marginBottom: "10px" }} />
+        </div>
+      </div>
+      <div
+        className="cloud"
+        style={{
+          top: "74vh",
+          left: "29vw",
+          transform: "scale(0.3) rotate(5deg)",
+          filter: "blur(10px)",
+        }}
+      >
+        <div
+          className="cloudHalf"
+          style={{ marginBottom: "-6rem", marginLeft: "-1rem" }}
+        >
+          <div className="cloudShape2" style={{ marginRight: "-15px" }} />
+          <div className="cloudShape3" />
+          <div className="cloudShape3" style={{ marginBottom: "-10px" }} />
+        </div>
+        <div className="cloudHalf">
+          <div className="cloudShape4" />
+          <div className="cloudShape1" />
+          <div className="cloudShape1" />
+          <div className="cloudShape1" style={{ marginBottom: "10px" }} />
+        </div>
+      </div>
+      <div
+        className="cloud"
+        style={{
+          top: "64vh",
+          left: "48vw",
+          transform: "scale(0.4) rotate(-5deg)",
+          filter: "blur(10px)",
+        }}
+      >
+        <div
+          className="cloudHalf"
+          style={{ marginBottom: "-6rem", marginLeft: "-1rem" }}
+        >
+          <div className="cloudShape2" style={{ marginRight: "-15px" }} />
+          <div className="cloudShape3" />
+          <div className="cloudShape3" style={{ marginBottom: "-10px" }} />
+        </div>
+        <div className="cloudHalf">
+          <div className="cloudShape4" />
+          <div className="cloudShape1" />
+          <div className="cloudShape1" />
+          <div className="cloudShape1" style={{ marginBottom: "10px" }} />
+        </div>
+      </div>
+      <div
+        className="cloud"
+        style={{
+          top: "96vh",
+          left: "70vw",
+          transform: "scale(1.5) rotate(-23deg)",
+          filter: "blur(2px)",
+        }}
+      >
+        <div
+          className="cloudHalf"
+          style={{ marginBottom: "-6rem", marginLeft: "-1rem" }}
+        >
+          <div className="cloudShape2" style={{ marginRight: "-15px" }} />
+          <div className="cloudShape3" />
+          <div className="cloudShape3" style={{ marginBottom: "-10px" }} />
+        </div>
+        <div className="cloudHalf">
+          <div className="cloudShape4" />
+          <div className="cloudShape1" />
+          <div className="cloudShape1" />
+          <div className="cloudShape1" style={{ marginBottom: "10px" }} />
+        </div>
+      </div>
+      <div
+        className="cloud"
+        style={{
+          top: "40vh",
+          left: "70vw",
+          transform: "scale(0.5) rotate(-10deg)",
+          filter: "blur(6px)",
+        }}
+      >
+        <div
+          className="cloudHalf"
+          style={{ marginBottom: "-6rem", marginLeft: "-1rem" }}
+        >
+          <div className="cloudShape2" style={{ marginRight: "-15px" }} />
+          <div className="cloudShape3" />
+          <div className="cloudShape3" style={{ marginBottom: "-10px" }} />
+        </div>
+        <div className="cloudHalf">
+          <div className="cloudShape4" />
+          <div className="cloudShape1" />
+          <div className="cloudShape1" />
+          <div className="cloudShape1" style={{ marginBottom: "10px" }} />
+        </div>
+      </div>
+      <div
+        className="cloud"
+        style={{
+          top: "51vh",
+          left: "83vw",
+          transform: "scale(0.7) rotate(-8deg)",
+          filter: "blur(6px)",
+        }}
+      >
+        <div
+          className="cloudHalf"
+          style={{ marginBottom: "-6rem", marginLeft: "-1rem" }}
+        >
+          <div className="cloudShape2" style={{ marginRight: "-15px" }} />
+          <div className="cloudShape3" />
+          <div className="cloudShape3" style={{ marginBottom: "-10px" }} />
+        </div>
+        <div className="cloudHalf">
+          <div className="cloudShape4" />
+          <div className="cloudShape1" />
+          <div className="cloudShape1" />
+          <div className="cloudShape1" style={{ marginBottom: "10px" }} />
+        </div>
+      </div>
     </main>
   );
 }
