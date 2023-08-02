49e4d0b117d6ae2028e577bc1a718c489e32acb5
seoeongeueun <erin9901@gmail.com>
2023-08-02 17:38:48 +0900

    first balloon

--- client/src/App.js
+++ client/src/App.js
@@ -1,5 +1,13 @@
 function App() {
-  return <main />;
+  return (
+    <main>
+      <div className="balloon" style={{ "--initial-rotation": "150deg" }}>
+        <div className="roundShape">
+          <div className="balloonTip" />
+        </div>
+      </div>
+    </main>
+  );
 }
 
 export default App;
